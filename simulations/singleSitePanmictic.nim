01db3dc57b74f8b95b5fb51023825b6d281ec29e
Kerry Cobb <cobbkerry@gmail.com>
2022-05-03 11:47:36 -0500

    Add CLI to steppingn stone sim, minor changes to both sims

--- simulations/singleSitePanmictic.nim
+++ simulations/singleSitePanmictic.nim
@@ -1,3 +1,6 @@
+## Simulate heterozygote disadvantage in a single panmictic population of 
+## randomly mating hermaphrodites with recombination
+
 import ../src/ancestry
 import ../src/junctions
 import ../src/singleSiteHaplotype
@@ -5,47 +8,70 @@ import ../src/deme
 import std/random
 import std/strutils
 import std/sequtils
+import std/json
 
-let
-  generations = 100 
-  demeSize = 100
-  halfSize = demeSize div 2
-  length = 1_000_000
-  sitePos = 500_000
-  recombRate = 1e-8
-  recombProb = recombRate * (length-1).float
-  selection = rand(1.0)
-  outfile = "singleSitePanmictic-output.txt"
-
-var 
-  startDeme = concat(
-    newDemeWith(halfSize, singleSiteHaplotype(length, sitePos, ancSp1)),
-    newDemeWith(halfSize, singleSiteHaplotype(length, sitePos, ancSp2)))
-  endDeme = newDeme[Haplotype[SingleSite]](demeSize)
-
-for gen in 0 ..< generations:
-  endDeme.empty()
-  while endDeme.size < endDeme.cap:
-    let  
-      hap1 = sample(startDeme.members)
-      hap2 = sample(startDeme.members)
-    if hap1.data.allele == hap2.data.allele:
-      if recombProb < rand(1.0): 
-        endDeme.add(recombine(hap1, hap2, rand(1..hap1.len-1))) 
+proc mating(startDeme: Deme, endDeme: var Deme, recombProb, selection: float) = 
+  var
+    hap1 = sample(startDeme.members) 
+    hap2 = sample(startDeme.members) 
+  if hap1.data.allele == hap2.data.allele: 
+    if rand(1.0) < recombProb:
+      endDeme.add(recombine(hap1, hap2, rand(hap1.len-1)))
+    else:
+      endDeme.add(hap1)
+  else:
+    if rand(1.0) < selection:
+      if rand(1.0) < recombProb:
+        endDeme.add(recombine(hap1, hap2, rand(hap1.len-1)))
       else:
         endDeme.add(hap1)
-    else:
-      if selection < rand(1.0): 
-        if recombProb < rand(1.0): 
-          let bp = rand(length-1)
-          endDeme.add(recombine(hap1, hap2, rand(1..hap1.len-1)))
-        else:
-          endDeme.add(hap1)
-  startDeme = endDeme 
 
-var output = open(outfile, fmWrite)
-for i in endDeme.members:
-  output.writeLine(i.junctions.join(","))
-output.close()
+proc simulation(generations, sampleSize, demeSize, length, position: int, recombRate: float, 
+    seed: int, outfile: string) = 
+  ## generations: number of generations
+  ## demeSize: size of deme
+  ## length: length of genomic region simulated
+  ## position: position within genomic region under selection
+  ## recombRate: per base probability of recombination occuring
+  ## seed: starting seed
+  ## outfile: output file path
 
+  assert demeSize mod 2 == 0 # Deme size must be even
+  assert position < length # Position must be smaller than the length
+  
+  randomize(seed)
+  let
+    halfSize = demeSize div 2
+    recombProb = recombRate * (length).float
+    selection = rand(1.0)
+  
+  var 
+    startDeme = concat(
+      newDemeWith(halfSize, singleSiteHaplotype(length, position, ancSp1)),
+      newDemeWith(halfSize, singleSiteHaplotype(length, position, ancSp2)))
+    endDeme = newDeme[Haplotype[SingleSite]](demeSize)
+  
+  for gen in 0 ..< generations:
+    endDeme.empty()
+    while endDeme.size < endDeme.cap:
+      mating(startDeme, endDeme, recombProb, selection)
+    startDeme = endDeme 
 
+  var 
+    output = open(outfile, fmWrite)
+    metaData = %*{
+      "seed": seed, 
+      "selection": selection,
+      "generations": generations, 
+      "deme size": demeSize,
+      "length": length,
+      "site position": position,
+      "recombination rate": recombRate,
+    } 
+  output.writeLine($metaData)
+  for i in 0..sampleSize-1:
+    output.writeLine(endDeme.members[i].junctions.join(","))
+  output.close()
+
+when isMainModule:
+  import cligen; dispatch(simulation)
--- simulations/singleSiteSteppingStone.nim
+++ simulations/singleSiteSteppingStone.nim
@@ -1,3 +1,6 @@
+## Simulate heterozygote disadvantage with a stepping stone model with demes of 
+## randomly mating hermaphrodites with recombination
+
 import ../src/ancestry
 import ../src/junctions
 import ../src/singleSiteHaplotype
@@ -7,57 +10,61 @@ import std/strutils
 import std/sequtils
 import std/json
 
-proc mating(source: Deme, recipient: var Deme, recombination, selection: float): bool = 
+proc mating(startDeme: Deme, endDeme: var Deme, recombProb, selection: float): bool = 
   var
-    hap1 = sample(source.members) 
-    hap2 = sample(source.members) 
+    hap1 = sample(startDeme.members) 
+    hap2 = sample(startDeme.members) 
   if hap1.data.allele == hap2.data.allele: 
-    if rand(1.0) < recombination:
-      recipient.add(recombine(hap1, hap2, rand(hap1.len-1)))
+    if rand(1.0) < recombProb:
+      endDeme.add(recombine(hap1, hap2, rand(hap1.len-1)))
     else:
-      recipient.add(hap1)
+      endDeme.add(hap1)
     result = true
   else:
     if rand(1.0) < selection:
-      if rand(1.0) < recombination:
-        recipient.add(recombine(hap1, hap2, rand(hap1.len-1)))
+      if rand(1.0) < recombProb:
+        endDeme.add(recombine(hap1, hap2, rand(hap1.len-1)))
       else:
-        recipient.add(hap1)
+        endDeme.add(hap1)
       result = true
     else:
       result = false
 
-proc migration(source: Deme, recipient: var Deme, nMigrants: int, recombination, selection: float) = 
+proc migration(source: Deme, recipient: var Deme, nMigrants: int, recombProb, selection: float) = 
   var migCnt = 0
   while migCnt < nMigrants:
-    var success = mating(source, recipient, recombination, selection)
+    var success = mating(source, recipient, recombProb, selection)
     if success:
       migCnt += 1
 
-proc simulation(outfile: string, seed=1234) = 
+proc simulation(generations, demeNum, demeSize, length, position: int, recombRate: float, 
+    seed: int, outfile: string) = 
+  ## generations: number of generations
+  ## demeNum: number of demes in stepping stone model
+  ## demeSize: size of demes
+  ## length: length of genomic region simulated
+  ## position: position within genomic region under selection
+  ## recombRate: per base probability of recombination occuring
+  ## seed: starting seed
+  ## outfile: output file path
+
+  assert demeNum mod 2 == 0 # Deme number must be even
+  assert position < length # Position must be smaller than the length
   randomize(seed)
-  let 
-    generations = 100 
-    demeSize = 100
-    demeNum = 100
-    length = 100_000
-    sitePos = 50_000
-    recombRate = 1e-8 
-
-    recombination = recombRate * (length).float
+  let
+    recombProb = recombRate * (length).float
     selection = rand(1.0) # Probability that a heterozygote will be inherited
-
   var
     startDemes = newSeqWith(demeNum, newDeme[Haplotype[SingleSite]](demeSize))
     endDemes = newSeqWith(demeNum, newDeme[Haplotype[SingleSite]](demeSize))
 
   # Populate sp1 Demes
   for deme in 0 ..< demeNum div 2:
-    startDemes[deme] = newDemeWith(demeSize, singleSiteHaplotype(length, sitePos, ancSp1)) 
+    startDemes[deme] = newDemeWith(demeSize, singleSiteHaplotype(length, position, ancSp1)) 
   
   # Populate sp2 Demes
   for deme in demeNum div 2 ..< demeNum:
-    startDemes[deme] = newDemeWith(demeSize, singleSiteHaplotype(length, sitePos, ancSp2)) 
+    startDemes[deme] = newDemeWith(demeSize, singleSiteHaplotype(length, position, ancSp2)) 
   
   for gen in 0 ..< generations:
     # Clear endDeme to accept new haplotypes
@@ -65,28 +72,28 @@ proc simulation(outfile: string, seed=1234) =
       endDemes[i].empty()
   
     # Migration from source to edge
-    endDemes[0].add(singleSiteHaplotype(length, sitePos, ancSp1))
-    endDemes[0].add(singleSiteHaplotype(length, sitePos, ancSp1))
-    endDemes[demeNum - 1].add(singleSiteHaplotype(length, sitePos, ancSp2))
-    endDemes[demeNum - 1].add(singleSiteHaplotype(length, sitePos, ancSp2))
+    endDemes[0].add(singleSiteHaplotype(length, position, ancSp1))
+    endDemes[0].add(singleSiteHaplotype(length, position, ancSp1))
+    endDemes[demeNum - 1].add(singleSiteHaplotype(length, position, ancSp2))
+    endDemes[demeNum - 1].add(singleSiteHaplotype(length, position, ancSp2))
   
     # Migration from neighbor to edge
-    migration(startDemes[1], endDemes[0], 2, recombination, selection)
-    migration(startDemes[0], endDemes[1], 2, recombination, selection)
+    migration(startDemes[1], endDemes[0], 2, recombProb, selection)
+    migration(startDemes[0], endDemes[1], 2, recombProb, selection)
   
     # Migration from edge to neighbor
-    migration(startDemes[demeNum-2], endDemes[demeNum-1], 2, recombination, selection)
-    migration(startDemes[demeNum-1], endDemes[demeNum-2], 2, recombination, selection)
+    migration(startDemes[demeNum-2], endDemes[demeNum-1], 2, recombProb, selection)
+    migration(startDemes[demeNum-1], endDemes[demeNum-2], 2, recombProb, selection)
   
     # Migration between non edge demes
     for i in 1 ..< demeNum - 2:
-      migration(startDemes[i], endDemes[i+1], 2, recombination, selection)
-      migration(startDemes[i+1], endDemes[i], 2, recombination, selection)
+      migration(startDemes[i], endDemes[i+1], 2, recombProb, selection)
+      migration(startDemes[i+1], endDemes[i], 2, recombProb, selection)
   
     # Sample from within deme to populate next generation 
     for i in 0 ..< demeNum:
       while endDemes[i].size < endDemes[i].cap:
-        discard mating(startDemes[i], endDemes[i], recombination, selection)
+        discard mating(startDemes[i], endDemes[i], recombProb, selection)
     startDemes = endDemes
   
   var 
@@ -98,7 +105,7 @@ proc simulation(outfile: string, seed=1234) =
       "deme size": demeSize,
       "deme number": demeNum,
       "length": length,
-      "site position": sitePos,
+      "site position": position,
       "recombination rate": recombRate,
     }
   output.writeLine($metaData)
